64aa07d896c424904ce0de817bc56905632eb286
JoshScarberry <37409482+JoshScarberry@users.noreply.github.com>
2018-03-16 16:58:35 -0400

    Lots of Changes
    got multple pages working. More formatting. login  working

--- capstoneProject/irbSite/views.py
+++ capstoneProject/irbSite/views.py
@@ -3,4 +3,26 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
 	#return HttpResponse('Home page works!!!')
-	return render(request, 'irbSite/irb_login.html')
+
+	# example variables
+	name = 'madeUpName'
+	anything = 'anyvalue'
+
+	# example of args to pass as dictionaries
+	args = {'myname': name, 'anyvalue': anything}
+	return render(request, 'irbSite/irb_login.html', args)
+
+def irb_form(request):
+	return render(request, 'irbSite/irb_form.html')
+
+def irb_register(request):
+	return render(request, 'irbSite/irb_register.html')
+
+def irb_reset(request):
+	return render(request, 'irbSite/irb_reset.html')
+
+def admin(request):
+	return render(request, 'irbSite/admin.html')
+
+def confirmation(request):
+	return render(request, 'irbSite/confirmation.html')
