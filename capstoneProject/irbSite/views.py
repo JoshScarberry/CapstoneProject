315fd832362289a7a3704de87bff93ebd7962cdb
JoshScarberry <37409482+JoshScarberry@users.noreply.github.com>
2018-04-08 22:59:30 -0400

    fixed project edit
    Still need custom vals for save vs submit. Still need custom view for admin with User and Project form data.

--- capstoneProject/irbSite/views.py
+++ capstoneProject/irbSite/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView, TemplateView, ListView, DetailView, View, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -9,7 +10,6 @@ from irbSite.models import Project, User
 
 
 # Create your views here.
-#class LoginView
 
 class Index(LoginRequiredMixin,ListView):
     template_name = 'irbSite/index.html'
@@ -30,7 +30,6 @@ class Form(LoginRequiredMixin,CreateView):
     form_class = ProjectForm
     success_url = reverse_lazy('irbSite:index')
 
-    # https://stackoverflow.com/questions/10382838/how-to-set-foreignkey-in-createview
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(Form, self).form_valid(form)
@@ -41,29 +40,29 @@ class EditProject(LoginRequiredMixin, UpdateView):
     form_class = ProjectForm
     success_url = reverse_lazy('irbSite:index')
 
-    # working for 1 project object
-    def get_object(self):
-        return Project.objects.get(user=self.request.user)
+    queryset = Project.objects.all()
+    def edit_object(queryset, pk):
+        editPK = get_object_or_404(Project, id=pk)
+        project_list = Project.objects.filter(project_id=editPK)
 
-    #def get_object(self):
-    #    return Project.objects.get(pk=self.request.user)
 
-    #def get_object(self, queryset=get_queryset):
-    #    return self.request.user
-
-    #def get_object(self, *args, **kwargs):
-    #    user = get_object(User, pk=self.kwargs['pk'])
-    #    return user.userprofile
-
-class IrbAdmin(LoginRequiredMixin, TemplateView):
+class IrbAdmin(LoginRequiredMixin, ListView):
     template_name = 'irbSite/admin_forms.html'
     form_class = AdminForm
 
-    def get_queryset(self):
-        return Project.objects.filter(is_complete=True)
+    def get_context_data(self, **kwargs):
+        context = super(MultipleModelView, self).get_context_data(**kwargs)
+        context['project'] = Project.objects.get(is_comlete=True)
+        context['user'] = User.objects.all()
+        return context
+
+    #def get_queryset(self):
+    #    return Project.objects.filter(is_complete=True)
 
 #https://stackoverflow.com/questions/12187751/django-pass-multiple-models-to-one-template
     #queryset = Project.objects.filter(is_complete=True)
+    #def get_user_names(queryset):
+    #    userNames = User.objects.filter(id=querryset.user)
 
     #def users(self):
     #    return User.objects.all()
